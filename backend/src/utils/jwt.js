56a4406e54a16a9b6ef0f42287c320a97a5d1571
vntayemm <3134427+vntayemm@users.noreply.github.com>
2025-06-29 17:08:39 +0700

    update expired time to config

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -2,5 +2,5 @@ import jwt from 'jsonwebtoken';
 import config from '../config.js';
 
 const SECRET = config.JWT_SECRET;
-export const signToken = (payload) => jwt.sign(payload, SECRET, { expiresIn: '1d' });
+export const signToken = (payload) => jwt.sign(payload, SECRET, { expiresIn: config.JWT_EXPIRES_IN });
 export const verifyToken = (token) => jwt.verify(token, SECRET); 
