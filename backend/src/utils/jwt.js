841074dc624d583d90c2167540ade41fc8e309a0
vntayemm <3134427+vntayemm@users.noreply.github.com>
2025-06-29 17:05:55 +0700

    update secret token

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken';
-const SECRET = process.env.JWT_SECRET || 'SECRET';
+import config from '../config.js';
+
+const SECRET = config.JWT_SECRET;
 export const signToken = (payload) => jwt.sign(payload, SECRET, { expiresIn: '1d' });
 export const verifyToken = (token) => jwt.verify(token, SECRET); 
